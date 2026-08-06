f80517c685d517072d82e0824e5a102c8d8e4f4f
Alberto Dgedge <albertoveloz234@gmail.com>
2026-08-06 09:12:04 +0200

    path error fixed

--- routes/itemFolhaRoutes.js
+++ routes/itemFolhaRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const itemFolhaController = require('./../controllers/itemFolhaController');
+const itemFolhaController = require('./../controllers/ItemFolhaController');
 const authController = require('./../controllers/authController');
 
 const router = express.Router();
